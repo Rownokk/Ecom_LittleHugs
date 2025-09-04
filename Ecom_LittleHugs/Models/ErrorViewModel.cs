3edf710401a5bbd3338e7452b3dff002640bac12
Rownok Jahan Mowmita <151154578+Rownokk@users.noreply.github.com>
2025-09-05 00:50:43 +0600

    Add project files.

---
+++ Ecom_LittleHugs/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ecom_LittleHugs.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
