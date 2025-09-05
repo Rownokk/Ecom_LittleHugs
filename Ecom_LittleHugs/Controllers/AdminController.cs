ddd9f001d615abbf46e2f240f0e20d263e9b962a
Rownok Jahan Mowmita <151154578+Rownokk@users.noreply.github.com>
2025-09-05 12:39:36 +0600

    Login Page

---
+++ Ecom_LittleHugs/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ecom_LittleHugs.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+           return View();
+        }
+    }
+}
