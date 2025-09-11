e4b88f68cf3581cbd5e514e7165a527080d673fb
jamila143 <jamila.cse.20220104143@aust.edu>
2025-09-11 19:31:04 +0600

    front-end  Home page

---
+++ Ecom_LittleHugs/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using Ecom_LittleHugs.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+
+namespace Ecom_LittleHugs.Controllers
+{
+    public class CustomerController : Controller
+    { private myContext _context;
+        public CustomerController(myContext context)
+        {
+            _context = context;
+
+        }
+        public IActionResult Index()
+        {
+
+            List<Category> category = _context.tbl_category.ToList();
+            ViewData["category"] = category;
+            return View();
+        }
+    }
+}
